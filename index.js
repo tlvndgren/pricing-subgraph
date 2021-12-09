3c0fb1f2eb1e521776a6a79079a447702decf691
Tyler Lundgren <tylerlundgren@yahoo.com>
2021-12-08 19:01:34 -0800

    Adjusting comments and fixing resolver.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// External Dependencies
 const fs = require("fs");
 const { ApolloServer, gql } = require("apollo-server");
 const { buildSubgraphSchema } = require("@apollo/federation")
+
+// Internal Dependencies
 const typeDefs = gql(fs.readFileSync("./prices.graphql", 'utf8'));
 const prices = require("./data/prices.js");
 
+// Variable Definitions
 const port = process.env.PORT || 4002
 
 const resolvers = {
@@ -22,10 +26,11 @@ const resolvers = {
     }
 };
 
+// Apollo Server Setup
 const server = new ApolloServer({
     schema: buildSubgraphSchema([{ typeDefs, resolvers }]),
-  });
+});
   
-  server.listen({ port }).then(({ url }) => {
+server.listen({ port }).then(({ url }) => {
     console.log(`Prices service ready at ${url}`);
-  });
+});
