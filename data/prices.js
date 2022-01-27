5adf344b3c489ff9ff9668cbfe5e6ab85b9a945a
Tyler Lundgren <tylerlundgren@yahoo.com>
2022-01-27 13:23:37 -0800

    Updating price to create error.

--- data/prices.js
+++ data/prices.js
@@ -2,77 +2,77 @@ const prices = [
   {
     "id": 1,
     "usdPrice": 29.67,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 2,
     "usdPrice": 48.84,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 3,
     "usdPrice": 35.99,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 4,
     "usdPrice": 14.26,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 5,
     "usdPrice": 99.3,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 6,
     "usdPrice": 85.48,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 7,
     "usdPrice": 53.72,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 8,
     "usdPrice": 65.08,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 9,
     "usdPrice": 64.46,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 10,
     "usdPrice": 44.36,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 11,
     "usdPrice": 0.93,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 12,
     "usdPrice": 57.38,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 13,
     "usdPrice": 9.46,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 14,
     "usdPrice": 75.18,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   },
   {
     "id": 15,
     "usdPrice": 35.19,
-    "priceDecorator": "$"
+    "priceDecorators": "$"
   }
 ]
 
