9272aa6ef8d5396023f5f1fd65ab731cfecc159b
Tyler Lundgren <tylerlundgren@deadfallwork.lan>
2024-06-06 15:01:39 -0600

    Fixing referenceresolver.

--- index.js
+++ index.js
@@ -16,65 +16,84 @@ import { prices } from "./data/prices.js";
 // Variable Definitions
 const port = process.env.PORT || 4002
 
+
 const resolvers = {
     Price: {
-        async __resolveReference(object) {
-            console.log("reference.price")
-            // return prices.find((price) => price.id === parseInt(object.id, 10));
-            const delay = new Promise((resolve) => {
-                    setTimeout(() => {
-                        resolve(prices.find((price) => price.id == parseInt(object.id, 10)));
-                    }, 2000);
-            });
-        const priceResult = await delay();
-        console.log("priceResult:", priceResult)
-        return priceResult;
-        }
+        __resolveReference(object) {
+            console.log("__reference.price")
+            return prices.find((price) => price.id === parseInt(object.id, 10));
+        },
     },
     Query: {
-        async price(_, { id }) {
+        price(_, { id }) {
             console.log("query.price")
-            function resolveAfter2Seconds() {
-                return new Promise((resolve) => {
-                  setTimeout(() => {
-                    resolve(prices.find((price) => price.id == parseInt(id, 10)));
-                  }, 2000);
-                });
-            }
-            async function asyncCall() {
-                console.log('calling');
-                const result = await resolveAfter2Seconds();
-                console.log(result);
-                return result
-            }
-            returnPrice = asyncCall();
-            console.log("returnPrice:", returnPrice)
-            return returnPrice
             return prices.find((price) => price.id === parseInt(id, 10));
         },
         prices() {
             console.log("query.prices")
-            let returnPrices;
-            function resolveAfter2Seconds() {
-                return new Promise((resolve) => {
-                  setTimeout(() => {
-                    resolve(prices);
-                  }, 2000);
-                });
-            }
-            async function asyncCall() {
-                console.log('calling');
-                const result = await resolveAfter2Seconds();
-                console.log(result);
-                return result
-            }
-            returnPrices = asyncCall();
-            console.log("returnPrices:", returnPrices)
-            return returnPrices
-            // return prices
+            return prices
         }
     }
 };
+// const resolvers = {
+//     Price: {
+//         async __resolveReference(object) {
+//             console.log("reference.price")
+//             // return prices.find((price) => price.id === parseInt(object.id, 10));
+//             const delay = new Promise((resolve) => {
+//                     setTimeout(() => {
+//                         resolve(prices.find((price) => price.id == parseInt(object.id, 10)));
+//                     }, 2000);
+//             });
+//         const priceResult = await delay();
+//         console.log("priceResult:", priceResult)
+//         return priceResult;
+//         }
+//     },
+//     Query: {
+//         async price(_, { id }) {
+//             console.log("query.price")
+//             function resolveAfter2Seconds() {
+//                 return new Promise((resolve) => {
+//                   setTimeout(() => {
+//                     resolve(prices.find((price) => price.id == parseInt(id, 10)));
+//                   }, 2000);
+//                 });
+//             }
+//             async function asyncCall() {
+//                 console.log('calling');
+//                 const result = await resolveAfter2Seconds();
+//                 console.log(result);
+//                 return result
+//             }
+//             returnPrice = asyncCall();
+//             console.log("returnPrice:", returnPrice)
+//             return returnPrice
+//             return prices.find((price) => price.id === parseInt(id, 10));
+//         },
+//         prices() {
+//             console.log("query.prices")
+//             let returnPrices;
+//             function resolveAfter2Seconds() {
+//                 return new Promise((resolve) => {
+//                   setTimeout(() => {
+//                     resolve(prices);
+//                   }, 2000);
+//                 });
+//             }
+//             async function asyncCall() {
+//                 console.log('calling');
+//                 const result = await resolveAfter2Seconds();
+//                 console.log(result);
+//                 return result
+//             }
+//             returnPrices = asyncCall();
+//             console.log("returnPrices:", returnPrices)
+//             return returnPrices
+//             // return prices
+//         }
+//     }
+// };
 
 // Apollo Server Setup
 const server = new ApolloServer({ 
